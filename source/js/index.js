8b8a66c72803e07d66cce6647924a34c41b15a93
Victor Tang <victortang1992@gmail.com>
2017-04-01 23:30:08 -0700

    use google font

--- source/js/index.js
+++ source/js/index.js
@@ -3,16 +3,4 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main(){
-    var MontserratBold = new FontFaceObserver('MontserratBold');
-    MontserratBold.load().then(function () {
-        document.body.classList.add("MontserratBold");
-    }, function () {
-        console.log('Font MontserratBold is not available');
-    });
-    var MuseoSlab500 = new FontFaceObserver('MuseoSlab500');
-    MuseoSlab500.load().then(function () {
-        document.body.classList.add("MuseoSlab500");
-    }, function () {
-        console.log('Font MuseoSlab500 is not available');
-    });
 };
