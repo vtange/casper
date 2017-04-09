3d7633d77044762c1efdeb155c7fdb51fc976895
Victor Tang <victortang1992@gmail.com>
2017-04-09 16:01:49 -0700

    multiimage load, scatter

--- source/js/slideshow.js
+++ source/js/slideshow.js
@@ -11,15 +11,14 @@ var _SS = new Slideshow(document.getElementById("slideshow"));
 
 window.addEventListener("resize",function(){
     if(_SS.currentIdx != null)
-        _SS.load(_SS.slides[_SS.currentIdx]);
+        _SS.load(_SS.currentIdx);
 });
 
-function loadToSlideshow(img){
+function loadToSlideshow(img, idx){
     var src = img.getAttribute("src");
     var realsrc = img.getAttribute("real-src");
     var ctr_x = img.getAttribute("ctr-x");
     var ctr_y = img.getAttribute("ctr-y");
-    var idx = _SS.slides.length;
 
     if(!_SS.slides[idx])
     {
@@ -29,8 +28,6 @@ function loadToSlideshow(img){
     _SS.slides[idx].ctr_x = ctr_x;
     _SS.slides[idx].ctr_y = ctr_y;
     img.style.display = "none";
-    
-    _SS.load(_SS.slides[0]);
 }
 
 function loadMulti(imgSrc, idx){
@@ -39,14 +36,13 @@ function loadMulti(imgSrc, idx){
         _SS.slides[idx] = _SS.createSlide("multi");
     }
 
-    _SS.slides[idx].arrSrcs.push(imgSrc);
+    _SS.slides[idx].arrImgs.push({src:imgSrc,img:null});
 }
 
 Slideshow.prototype.createSlide = function(type){
     if(type == "multi")
     {
         return {
-            arrSrcs: [],
             arrImgs: []
         }
     }
@@ -90,33 +86,102 @@ Slideshow.prototype.resize = function(img){
 }
 
 Slideshow.prototype.load = function(index){
+    var tis = this;
     var slide = this.slides[index];
+    var isReload = this.currentIdx == index;
     this.currentIdx = index;
 
-    this.centerBias.x = parseInt(slide.ctr_x,10);
-    this.centerBias.y = parseInt(slide.ctr_y,10);
-    if(slide.real)
+    if(slide.mini)
     {
-        this.draw(slide.real);
+        //set centerBias for slide offset
+        this.centerBias.x = parseInt(slide.ctr_x,10);
+        this.centerBias.y = parseInt(slide.ctr_y,10);
+
+        if(slide.real)
+        {
+            this.drawSingle(slide.real);
+        }
+        else
+        {
+            this.drawSingle(slide.mini);
+
+            //begin loading real size image
+            slide.real = new Image();
+            slide.real.src = slide.mini.getAttribute("real-src");
+            slide.real.onload = function(){
+                this.load(slide);
+            }.bind(this);
+        }
     }
     else
     {
-        this.draw(slide.mini);
+        //standard canvas width
+        this.canvas.width = this.canvas.clientWidth;
+        this.canvas.height = this.canvas.clientHeight;
+        
+        //if first draw
+        if(!isReload)
+        {
+            slide.arrImgs.forEach(function(imgObj,index){
+                if(!imgObj.img)
+                {
+                    imgObj.img = new Image();
+                    imgObj.img.src = imgObj.src;
+                    imgObj.img.onload = function(){
+                        tis.drawMulti(imgObj,index);
+                    }
+                }
+                else
+                {
+                    tis.drawMulti(imgObj,index);
+                }
 
-        //begin loading real size image
-        slide.real = new Image();
-        slide.real.src = slide.mini.getAttribute("real-src");
-        slide.real.onload = function(real){
-            this.load(slide);
-        }.bind(this);
+            });
+        }
+        else
+        {
+
+
+        }
     }
 }
 
-Slideshow.prototype.draw = function(img)
+Slideshow.prototype.drawSingle = function(img)
 {
     //resize to prep for slides
     this.offsets = this.resize(img);
 
     //draw
     this.cxt.drawImage(img,this.offsets.x,this.offsets.y);
+}
+
+Slideshow.prototype.drawMulti = function(imgObj, index)
+{
+    var yPos = [400, 90, 195, 440, 300, 100, 450, 40, 345, 210, 500, 60];
+    var scale = [];
+
+    //load/animate images (275 tall 400 wide)
+    window.setTimeout(function(){
+        //save blank state
+        this.cxt.save();
+
+        //set transforms
+        var now = Date.now();
+        var animDurr = 1500;//1.5sec
+        this.cxt.translate(0,-100-yPos[index]);
+        this.cxt.shadowColor = 'rgba(0,0,0,0.1)';
+        this.cxt.shadowBlur = 20;
+        this.cxt.shadowOffsetX = 15;
+        this.cxt.shadowOffsetY = 15;
+
+        //draw
+        this.cxt.drawImage(
+            imgObj.img,
+            this.canvas.width/this.slides[this.currentIdx].arrImgs.length*(index-0.5),
+            this.canvas.height-100
+        );
+
+        //rdy for next img
+        this.cxt.restore();
+    }.bind(this),60*index);
 }
