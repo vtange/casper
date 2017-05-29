1e5bba96a7d66db64394318f2964a99e844f32d0
Victor Tang <victortang1992@gmail.com>
2017-05-28 17:02:00 -0700

    script update for year, improved blur

--- source/js/portfolio.js
+++ source/js/portfolio.js
@@ -4,10 +4,11 @@ var portfolio = document.querySelector(".works-marker");
 var projects = document.getElementsByTagName("section")[0];
 if(!portfolio||!projects){return;}
 var gallery = portfolio.getElementsByTagName("img");
-function createPopup(x,y, title, text){
+function createPopup(x,y, title, year, text){
     clearPopup();
     var pop = document.createElement("div");
     var titleEl = document.createElement("h5");
+    var yearEl = document.createElement("span");
     var textEl = document.createElement("span");
     var arrow = document.createElement("i");
     var boxclose = document.createElement("a");
@@ -16,7 +17,11 @@ function createPopup(x,y, title, text){
     arrow.classList.add("arrow");
     arrow.classList.add("up");
     titleEl.innerHTML = title;
+    yearEl.innerHTML = " ("+year+")";
+    yearEl.style.fontSize = "2rem";
+    yearEl.style.color = "#bab2b6";
     textEl.innerHTML = text;
+    titleEl.appendChild(yearEl);
     pop.id = "work-desc-popup";
     pop.classList.add("work-desc-popup");
     pop.style.top = y+25+"px";
@@ -41,15 +46,16 @@ for(let i = gallery.length-1; i >= 0; i--)
     var img = gallery[i];
     var article = projects.querySelector("article[name="+img.getAttribute("alt")+"]");
     var title = article.getAttribute("title");
+    var year = article.getAttribute("year");
     var txt = article.innerText;
     (function(title,text){
         //prevent reference to last title, txt
         img.addEventListener("click",function(event){
-            createPopup(event.pageX, event.pageY, title, text);
+            createPopup(event.pageX, event.pageY, title, year, text);
+            event.stopPropagation();
         });
     }(title,txt));
-    img.addEventListener("blur",function(event){
-        clearPopup();
-    });
+    img.addEventListener("blur",clearPopup);
 }
+document.body.addEventListener("click",clearPopup);
 })();
